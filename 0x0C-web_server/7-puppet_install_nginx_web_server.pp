f7cfcb9a286aefe3e07790e0f306f5842d8886fc
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-22 13:54:50 -0500

    Adding 7-puppet_install_nginx_web_server.pp file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# Script that instals and configures nginx on a server
+
+package { 'nginx':
+  ensure => present,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
