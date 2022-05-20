5d0f138f84f93788f8d2499a76f07f5b4e26cca1
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-20 15:34:01 -0500

    Fixing up 3-dictionary_of_list_of_dictionaries.py file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,42 +9,41 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/"
     url_user = url + "users"
-    url_todos = url + "todos"
+    url_todo = url + "todos"
 
     request_user = get(url_user)
-    request_todos = get(url_todos)
-
+    request_todo = get(url_todo)
     # Connection and have an access to the json
     try:
         jsuser = request_user.json()
-        jstodos = request_todos.json()
+        jstodo = request_todo.json()
     except ValueError:
         print("No Json")
 
-    # Assing values
-    if jsuser and jstodos:
+    # Assign values
+    if jsuser and jstodo:
         jsresult = {}
         user_names = {}
         for user in jsuser:
-            USER_ID = user.get("id")
-            USERNAME = user.get("username")
+            USER_ID = user.get('id')
+            USERNAME = user.get('username')
             jsresult[USER_ID] = []
             user_names[USER_ID] = USERNAME
 
         # create the value of the dict of the final json file
         # jslist = jsresult[USER_ID]
-        for task in jstodos:
-            TASK_TITLE = task.get("title")
-            TASK_COMPLETED_STATUS = task.get("completed")
+        for task in jstodo:
+            TASK_TITLE = task.get('title')
+            TASK_COMPLETED_STATUS = task.get('completed')
             # write the internal dict
             user_id = task.get("userId")
             taskdict = {"task": TASK_TITLE,
                         "completed": TASK_COMPLETED_STATUS,
                         "username": user_names.get(user_id)}
 
-            if jsresult(user_id) is not None:
+            if jsresult.get(user_id) is not None:
                 jsresult.get(user_id).append(taskdict)
 
         # generate the jsonfile
-        with open("todo_all_employees.json", "w", newline="") as jsonfile:
+        with open('todo_all_employees.json', 'w', newline='') as jsonfile:
             json.dump(jsresult, jsonfile)
