2028511ea06899377a7788e78b47bf4616dc6280
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-21 11:46:46 -0500

    Fixing up 2-recurse.py file

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,24 +5,29 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """Returns a list containing
-        the titles of all hot articles for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".format(
-        subreddit, after)
-    headers = {"User-Agent": "MyApp/0.0.1"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Return a list of titles of all hot articles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    params = {'after': after}
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code == 200:
         try:
             data = response.json().get("data")
-            children = data.get("children")
+            after = data.get("after", None)
+            children = data.get("children", None)
             for each in children:
-                hot_list.append(each.get("data").get("title"))
-            after = data.get("after")
-            if after is not None:
-                recurse(subreddit, hot_list, after)
+                hot_list.append(each.get("data").get("title", None))
         except Exception as e:
-            return hot_list
+            return None
     else:
+        return None
+
+    if after is None:
+        if len(hot_list) == 0:
+            return None
         return hot_list
 
     return recurse(subreddit, hot_list, after)
