4978ea83c62c4736ba0d4ec842399dbd0c8d54f0
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-21 11:17:59 -0500

    Fixing up 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers of a subreddit"""
+
     url = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={'user-agent': 'platform'},
+        headers={"User-Agent": "platform"},
         allow_redirects=False)
-
     if url.status_code == 200:
-        return int(url.json()).get("data").get("subscribers")
+        return int(url.json().get("data").get("subscribers"))
     else:
         return 0
